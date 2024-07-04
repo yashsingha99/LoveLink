b070a835cf20ea33530ef1bc99b4a9e4c9b076a0
Yash Singhal <singhaly914@gmail.com>
2024-07-04 14:50:11 +0530

    add user model

---
+++ backend/src/model/user.model.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const userSchema = await mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      require: true,
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    phone_no: {
+      type: String,
+      require: true,
+    },
+    qualification: [      // add more qualifications
+       {
+         type: String,
+         require: true,
+       },
+    ],
+    avatar : {
+        type : String,
+        default : ""
+    },
+    body : {            // here you make a object for user body structure 
+        type : Object  // like : skin color, any body demages, 
+    },                  
+    discription : {
+        type : String,
+        require : true
+    },
+    about : {
+        type : String,
+        require : true
+    },
+    DOB : {
+        type : Date,
+        require : true
+    },
+    Achievments : {
+        type : String,
+        require : true
+    },
+
+    place : {
+        type : String,
+        require : true
+    },
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = await mongoose.model("User", userSchema);
+module.exports = User
