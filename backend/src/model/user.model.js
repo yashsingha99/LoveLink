741dcfabdd4b64bcaa6574ad971b2b3b72930a9c
Yash Singhal <singhaly914@gmail.com>
2024-07-04 15:02:44 +0530

    add more attributes

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -1,47 +1,58 @@
 const mongoose = require('mongoose')
 const userSchema = await mongoose.Schema(
   {
+    
     fullName: {
       type: String,
       require: true,
     },
+
     username: {
       type: String,
       require: true,
     },
+
     email: {
       type: String,
       require: true,
     },
+
     phone_no: {
       type: String,
       require: true,
     },
+
     qualification: [      // add more qualifications
        {
          type: String,
          require: true,
        },
     ],
+
     avatar : {
         type : String,
         default : ""
     },
+
     body : {            // here you make a object for user body structure 
         type : Object  // like : skin color, any body demages, 
-    },                  
+    }, 
+
     discription : {
         type : String,
         require : true
     },
+
     about : {
         type : String,
         require : true
     },
+
     DOB : {
         type : Date,
         require : true
     },
+
     Achievments : {
         type : String,
         require : true
@@ -51,7 +62,20 @@ const userSchema = await mongoose.Schema(
         type : String,
         require : true
     },
-    
+
+    followers : [
+      {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+      }
+    ],
+
+    following : [
+      {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+      }
+    ]
   },
   {
     timestamps: true,
