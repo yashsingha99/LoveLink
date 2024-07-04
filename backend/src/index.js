ad0b6048054d5b97194f9746999815cde9d5c3fa
Yash Singhal <singhaly914@gmail.com>
2024-07-04 22:26:28 +0530

    add

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,21 @@
-const dotenv = require('dotenv')
+const dotenv  = require('dotenv')
+
+const mongoose  = require('mongoose')
+
 dotenv.config()
 
-const mongoose = require('mongoose')
+
 const URI = process.env.URI
-const connect = async () => {
-try {
-     const res  = await mongoose.connect( URI)
-     console.log("databasse connected");
-} catch (error) {
-    console.log("connection failed");
-}}
+const connectDb = async() => {
+    try {
+        const connect = await mongoose.connect(URI)
+        console.log("database connected");
+    } catch (error) {
+        console.log(error);
+    }
+}
+connectDb()
 
-connect();
 
-const app = require("app")
-app.listen(3000, () => console.log("server is running on PORT", 3000))
+const app = require("./app")
+app.listen(3000, console.log("server is running...."))
--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -1,7 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const userSchema = await mongoose.Schema(
   {
-
     fullName: {
       type: String,
       require: true,
@@ -17,9 +16,9 @@ const userSchema = await mongoose.Schema(
       require: true,
     },
 
-    password : {
-     type : String,
-     require : true
+    password: {
+      type: String,
+      require: true,
     },
 
     phone_no: {
@@ -27,66 +26,80 @@ const userSchema = await mongoose.Schema(
       require: true,
     },
 
-    qualification: [      // add more qualifications
-       {
-         type: String,
-         require: true,
-       },
+    qualification: [
+      // add more qualifications
+      {
+        type: String,
+        require: true,
+      },
     ],
 
-    avatar : {
-        type : String,
-        default : ""
+    avatar: {
+      type: String,
+      default: "",
     },
 
-    body : {            // here you make a object for user body structure 
-        type : Object  // like : skin color, any body demages, 
-    }, 
-
-    discription : {
-        type : String,
-        require : true
+    body: {
+      // here you make a object for user body structure
+      type: Object, // like : skin color, any body demages,
     },
 
-    about : {
-        type : String,
-        require : true
+    discription: {
+      type: String,
+      require: true,
     },
 
-    DOB : {
-        type : Date,
-        require : true
+    about: {
+      type: String,
+      require: true,
     },
 
-    Achievments : {
-        type : String,
-        require : true
+    DOB: {
+      type: Date,
+      require: true,
     },
 
-    place : {
-        type : String,
-        require : true
-    },
-
-    followers : [
+    Achievments: [
       {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User"
-      }
+        type: String,
+        require: true,
+      },
     ],
 
-    following : [
-      {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User"
-      }
-    ]
+    place: {
+      type: String,
+      require: true,
+    },
 
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+      },
+    ],
+
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+      },
+    ],
+
+    isMarried: {
+      type: Boolean,
+      required: true,
+    },
+
+    isDevorcedPerson: {
+      type: Boolean,
+      require: true,
+    },
   },
+
   {
     timestamps: true,
   }
 );
 
-const User = await mongoose.model("User", userSchema);
-module.exports = User
+const User = mongoose.model("User", userSchema);
+module.exports = User;
